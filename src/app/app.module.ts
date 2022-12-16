012aa6bbf5d9b95ca68688fa9db7c6ac3157e34e
jagadhis <jwaran78@gmail.com>
2022-12-17 01:52:48 +0530

    Added route with buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { TodosComponent } from './todos/todos.component';
 import { RoutingComponent } from './routing/routing.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,15 @@ import { RoutingComponent } from './routing/routing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'app-todos',component:TodosComponent},
+        {path: 'app-routing',component:RoutingComponent},
+        {path:'',redirectTo: '/app-todos',pathMatch:'full'}
+
+    
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
