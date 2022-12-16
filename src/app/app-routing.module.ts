a5d74ffb970a5df018163e08999cc51ac0785ec8
jagadhis <jwaran78@gmail.com>
2022-12-17 01:37:49 +0530

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
+import { RoutingComponent } from './routing/routing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app-todos',component:TodosComponent},
+    {path: 'app-routing',component:RoutingComponent}
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+exports:[RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TodosComponent } from './todos/todos.component';
+import { RoutingComponent } from './routing/routing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
    
-    TodosComponent
+    TodosComponent,
+         RoutingComponent
   ],
   imports: [
     BrowserModule,
