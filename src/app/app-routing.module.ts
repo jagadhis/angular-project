da087282e90b0a462d421975389c64d744a44c75
jagadhis <jwaran78@gmail.com>
2022-12-19 13:50:26 +0530

    Refactoring Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
-import { RoutingComponent } from './routing/routing.component';
 
-const routes: Routes = [
-  {
-    path: 'app-todos',component:TodosComponent},
-    {path: 'app-routing',component:RoutingComponent}
-
-  
-
-];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TodosComponent } from './todos/todos.component';
-import { RoutingComponent } from './routing/routing.component';
+
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
    
-    TodosComponent,
-         RoutingComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      {
-        path: 'app-todos',component:TodosComponent},
-        {path: 'app-routing',component:RoutingComponent},
-        {path:'',redirectTo: '/app-todos',pathMatch:'full'}
-
-    
-    ])
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
